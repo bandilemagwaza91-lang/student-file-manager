83dd3e83c193f2e16bfa81de18f5595b0f6a4f4f
Bandile Magwaza <bandile.magwaza91@gmail.com>
2026-01-16 23:37:11 +0200

    Update to my 2main.py file

--- 2main.py
+++ 2main.py
@@ -5,17 +5,49 @@ window=tk.Tk()
 window.title("Mini banking system")
 window.geometry("500x400")
 
-window2=tk.Tk()
-window2.title("sign in ")
-window2.geometry("500x400")
+Main_frame=tk.Frame(window)
+LogIn_frame=tk.Frame(window)
+Account_Creation_Frame=tk.Frame(window)
 
-window3=tk.Tk()
-window3.title(" log_in ")
-window3.geometry("500x400")
 
-def sign_in():
+
+Main_frame.pack(fill="both", expand=True)
+LogIn_frame.pack_forget()
+Account_Creation_Frame.pack_forget()
+
+
+
+
+def go_to_LogIn():
+  Main_frame.pack_forget()
+  LogIn_frame.pack(fill="both", expand=True)
      
 
-def log_in():
-   
-        
+def go_to_AccountCreation():
+    Main_frame.pack_forget()
+    Account_Creation_Frame.pack(fill="both", expand=True)
+
+# Main page 
+tk.Label(Main_frame,text="Hello", font=("Arial", 30)).pack(pady=20)
+
+tk.Button(Main_frame, text="Login", command= go_to_LogIn).pack(pady=15) 
+tk.Button(Main_frame, text="create Account", command=go_to_AccountCreation).pack(pady=15) 
+
+
+# login
+tk.Label(LogIn_frame, text="Bank Login", font=("Arial", 18)).pack(pady=20)
+
+tk.Label(LogIn_frame, text="Username").pack()
+username_entry = tk.Entry(LogIn_frame)
+username_entry.pack()
+
+tk.Label(LogIn_frame, text="PIN").pack()
+pin_entry = tk.Entry(LogIn_frame, show="*")
+pin_entry.pack()
+
+#tk.Button(LogIn_frame, text="Login", command= Account_Creation_Frame).pack(pady=15)   
+
+# creation of Account 
+
+
+window.mainloop()
